be1411ff1ace12d27a28c0c410f06304a1af967b
Kara <karab@princeton.edu>
2017-06-11 15:54:57 -0400

    Border colors

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-// initialization
-$(document).ready(function() {
-	if ($(window).height() <= 768) {
-			$('.main-div').addClass('limit-small');
-			$('.proj-img').addClass('img-responsive');
-			$('.proj-img-w').addClass('img-responsive');
-			$('.proj-img-bw').addClass('img-responsive');
-			$('.main-div').removeClass('limit-large');
-	})
-};
+// // initialization
+// $(document).ready(function() {
+// 	if ($(window).height() <= 768) {
+// 			$('.main-div').addClass('limit-small');
+// 			$('.proj-img').addClass('img-responsive');
+// 			$('.proj-img-w').addClass('img-responsive');
+// 			$('.proj-img-bw').addClass('img-responsive');
+// 			$('.main-div').removeClass('limit-large');
+// 	})
+// };
 
 	// if ($(window).height() <= 768) {
 	// 		$('.main-div').addClass('limit-small');
@@ -17,18 +17,32 @@ $(document).ready(function() {
 	// 		$('.main-div').removeClass('limit-large');
 	// };
 
-$(window).on('resize', function() {
-    if($(window).height() > 768) {
-        $('.main-div').addClass('limit-large');
-        $('.main-div').removeClass('limit-small');
-				$('.proj-img').removeClass('img-responsive');
-				$('.proj-img-w').removeClass('img-responsive');
-				$('.proj-img-bw').removeClass('img-responsive');
-    } else {
-        $('.main-div').addClass('limit-small');
+$(document).ready(function(){
+
+	if ($(window).height() <= 922) {
+			$('.main-div').addClass('limit-small');
+			$('.proj-img').addClass('img-responsive');
+			$('.proj-img-w').addClass('img-responsive');
+			$('.proj-img-bw').addClass('img-responsive');
+			$('.main-div').removeClass('limit-large');
+	};
+
+	$(window).on('resize', function() {
+	    if($(window).height() > 922) {
+	        $('.main-div').addClass('limit-large');
+	        $('.main-div').removeClass('limit-small');
+					$('.proj-img').removeClass('img-responsive');
+					$('.proj-img-w').removeClass('img-responsive');
+					$('.proj-img-bw').removeClass('img-responsive');
+					$("img").removeClass('img-responsive');
+
+	    } else {
+				$('.main-div').addClass('limit-small');
 				$('.proj-img').addClass('img-responsive');
 				$('.proj-img-w').addClass('img-responsive');
 				$('.proj-img-bw').addClass('img-responsive');
-        $('.main-div').removeClass('limit-large');
-    }
-});
+				$('.main-div').removeClass('limit-large');
+	    }
+	});
+	
+)};
