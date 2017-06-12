c773baf624d75fa1940b7a8c57c3b761941c59cc
Kara <karab@princeton.edu>
2017-06-11 23:48:15 -0400

    Resizing working not on initialization

--- js/index.js
+++ js/index.js
@@ -17,32 +17,69 @@
 	// 		$('.main-div').removeClass('limit-large');
 	// };
 
+// $(document).ready(function(){
+//
+// 	if ($(window).width() < 960) {
+//    console.log('Less than 960');
+// 	}
+// 	else {
+//    console.log('More than 960');
+// 	}
+//
+// 	if ($(window).width() > 922) {
+// 		$('.main-div').addClass('limit-large');
+// 		$('.main-div').removeClass('limit-small');
+// 		$('.proj-img').removeClass('img-responsive');
+// 		$('.proj-img-w').removeClass('img-responsive');
+// 		$('.proj-img-bw').removeClass('img-responsive');
+// 	};
+//
+// 	$(window).on('resize', function() {
+// 	    if($(window).height() > 922) {
+// 	        $('.main-div').addClass('limit-large');
+// 	        $('.main-div').removeClass('limit-small');
+// 					$('.proj-img').removeClass('img-responsive');
+// 					$('.proj-img-w').removeClass('img-responsive');
+// 					$('.proj-img-bw').removeClass('img-responsive');
+// 			}
+// 			else {
+// 				$('.main-div').addClass('limit-small');
+// 				$('.proj-img').addClass('img-responsive');
+// 				$('.proj-img-w').addClass('img-responsive');
+// 				$('.proj-img-bw').addClass('img-responsive');
+// 				$('.main-div').removeClass('limit-large');
+// 	    }
+// 	});
+//
+// )};
 $(document).ready(function(){
 
-	if ($(window).height() <= 922) {
+	if($(window).width() > 922) {
+		$('.main-div').addClass('limit-large');
+		$('.main-div').removeClass('limit-small');
+		$('.proj-img').removeClass('img-responsive');
+		$('.proj-img-w').removeClass('img-responsive');
+		$('.proj-img-bw').removeClass('img-responsive');
+	}
+	
+)};
+
+
+$(window).on('resize', function() {
+
+		if($(window).width() > 922) {
+				$('.main-div').addClass('limit-large');
+				$('.main-div').removeClass('limit-small');
+				$('.proj-img').removeClass('img-responsive');
+				$('.proj-img-w').removeClass('img-responsive');
+				$('.proj-img-bw').removeClass('img-responsive');
+		}
+		else {
 			$('.main-div').addClass('limit-small');
 			$('.proj-img').addClass('img-responsive');
 			$('.proj-img-w').addClass('img-responsive');
 			$('.proj-img-bw').addClass('img-responsive');
 			$('.main-div').removeClass('limit-large');
-	};
+		}
 
-	$(window).on('resize', function() {
-	    if($(window).height() > 922) {
-	        $('.main-div').addClass('limit-large');
-	        $('.main-div').removeClass('limit-small');
-					$('.proj-img').removeClass('img-responsive');
-					$('.proj-img-w').removeClass('img-responsive');
-					$('.proj-img-bw').removeClass('img-responsive');
-					$("img").removeClass('img-responsive');
-
-	    } else {
-				$('.main-div').addClass('limit-small');
-				$('.proj-img').addClass('img-responsive');
-				$('.proj-img-w').addClass('img-responsive');
-				$('.proj-img-bw').addClass('img-responsive');
-				$('.main-div').removeClass('limit-large');
-	    }
-	});
-	
-)};
+});
